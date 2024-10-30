d78e66902d6e9fdb328164b4f2636e0b7e80a7b8
Timofey Kuratov <nynesnayu@gmail.com>
2024-10-30 14:47:55 +0300

    Сделал 7 лабу без допов

---
+++ lab7/Form1.Designer.cs
@@ -0,0 +1,351 @@
+﻿namespace lab7
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle1 = new System.Windows.Forms.DataGridViewCellStyle();
+            System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle3 = new System.Windows.Forms.DataGridViewCellStyle();
+            System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle2 = new System.Windows.Forms.DataGridViewCellStyle();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.recordBook = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.fullName = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.department = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.specification = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.dateOfAdmission = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.group = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.comboBox3 = new System.Windows.Forms.ComboBox();
+            this.dateTimePicker1 = new System.Windows.Forms.DateTimePicker();
+            this.comboBox2 = new System.Windows.Forms.ComboBox();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.cueTextBox2 = new lab7.CueTextBox();
+            this.cueTextBox1 = new lab7.CueTextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.groupBox1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.AllowUserToAddRows = false;
+            this.dataGridView1.AllowUserToDeleteRows = false;
+            this.dataGridView1.AllowUserToResizeColumns = false;
+            this.dataGridView1.AllowUserToResizeRows = false;
+            this.dataGridView1.AutoSizeColumnsMode = System.Windows.Forms.DataGridViewAutoSizeColumnsMode.Fill;
+            dataGridViewCellStyle1.Alignment = System.Windows.Forms.DataGridViewContentAlignment.MiddleLeft;
+            dataGridViewCellStyle1.BackColor = System.Drawing.SystemColors.Control;
+            dataGridViewCellStyle1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            dataGridViewCellStyle1.ForeColor = System.Drawing.SystemColors.WindowText;
+            dataGridViewCellStyle1.SelectionBackColor = System.Drawing.SystemColors.Highlight;
+            dataGridViewCellStyle1.SelectionForeColor = System.Drawing.SystemColors.HighlightText;
+            dataGridViewCellStyle1.WrapMode = System.Windows.Forms.DataGridViewTriState.True;
+            this.dataGridView1.ColumnHeadersDefaultCellStyle = dataGridViewCellStyle1;
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.recordBook,
+            this.fullName,
+            this.department,
+            this.specification,
+            this.dateOfAdmission,
+            this.group});
+            this.dataGridView1.Location = new System.Drawing.Point(9, 10);
+            this.dataGridView1.Margin = new System.Windows.Forms.Padding(2);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.ReadOnly = true;
+            dataGridViewCellStyle3.Alignment = System.Windows.Forms.DataGridViewContentAlignment.MiddleLeft;
+            dataGridViewCellStyle3.BackColor = System.Drawing.SystemColors.Control;
+            dataGridViewCellStyle3.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            dataGridViewCellStyle3.ForeColor = System.Drawing.SystemColors.WindowText;
+            dataGridViewCellStyle3.SelectionBackColor = System.Drawing.SystemColors.Highlight;
+            dataGridViewCellStyle3.SelectionForeColor = System.Drawing.SystemColors.HighlightText;
+            dataGridViewCellStyle3.WrapMode = System.Windows.Forms.DataGridViewTriState.True;
+            this.dataGridView1.RowHeadersDefaultCellStyle = dataGridViewCellStyle3;
+            this.dataGridView1.RowHeadersWidth = 51;
+            this.dataGridView1.RowTemplate.Height = 24;
+            this.dataGridView1.Size = new System.Drawing.Size(988, 358);
+            this.dataGridView1.TabIndex = 0;
+            this.dataGridView1.MouseClick += new System.Windows.Forms.MouseEventHandler(this.dataGridView1_MouseClick);
+            // 
+            // recordBook
+            // 
+            this.recordBook.HeaderText = "Номер книжки";
+            this.recordBook.MinimumWidth = 6;
+            this.recordBook.Name = "recordBook";
+            this.recordBook.ReadOnly = true;
+            // 
+            // fullName
+            // 
+            this.fullName.HeaderText = "ФИО студента";
+            this.fullName.MinimumWidth = 6;
+            this.fullName.Name = "fullName";
+            this.fullName.ReadOnly = true;
+            // 
+            // department
+            // 
+            this.department.HeaderText = "Институт";
+            this.department.MinimumWidth = 6;
+            this.department.Name = "department";
+            this.department.ReadOnly = true;
+            // 
+            // specification
+            // 
+            this.specification.HeaderText = "Направление";
+            this.specification.MinimumWidth = 6;
+            this.specification.Name = "specification";
+            this.specification.ReadOnly = true;
+            // 
+            // dateOfAdmission
+            // 
+            dataGridViewCellStyle2.Format = "d";
+            dataGridViewCellStyle2.NullValue = null;
+            this.dateOfAdmission.DefaultCellStyle = dataGridViewCellStyle2;
+            this.dateOfAdmission.HeaderText = "Дата зачисления";
+            this.dateOfAdmission.MinimumWidth = 6;
+            this.dateOfAdmission.Name = "dateOfAdmission";
+            this.dateOfAdmission.ReadOnly = true;
+            // 
+            // group
+            // 
+            this.group.HeaderText = "Группа";
+            this.group.MinimumWidth = 6;
+            this.group.Name = "group";
+            this.group.ReadOnly = true;
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.BackColor = System.Drawing.Color.RosyBrown;
+            this.groupBox1.Controls.Add(this.label2);
+            this.groupBox1.Controls.Add(this.label1);
+            this.groupBox1.Controls.Add(this.comboBox3);
+            this.groupBox1.Controls.Add(this.dateTimePicker1);
+            this.groupBox1.Controls.Add(this.comboBox2);
+            this.groupBox1.Controls.Add(this.comboBox1);
+            this.groupBox1.Controls.Add(this.cueTextBox2);
+            this.groupBox1.Controls.Add(this.cueTextBox1);
+            this.groupBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.groupBox1.Location = new System.Drawing.Point(9, 376);
+            this.groupBox1.Margin = new System.Windows.Forms.Padding(2);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Padding = new System.Windows.Forms.Padding(2);
+            this.groupBox1.Size = new System.Drawing.Size(803, 88);
+            this.groupBox1.TabIndex = 1;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Информация";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label2.Location = new System.Drawing.Point(607, 56);
+            this.label2.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(73, 24);
+            this.label2.TabIndex = 8;
+            this.label2.Text = "Группа";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label1.Location = new System.Drawing.Point(445, 19);
+            this.label1.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(163, 24);
+            this.label1.TabIndex = 7;
+            this.label1.Text = "Дата зачисления";
+            // 
+            // comboBox3
+            // 
+            this.comboBox3.Font = new System.Drawing.Font("Microsoft Sans Serif", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.comboBox3.FormattingEnabled = true;
+            this.comboBox3.Location = new System.Drawing.Point(680, 54);
+            this.comboBox3.Margin = new System.Windows.Forms.Padding(2);
+            this.comboBox3.Name = "comboBox3";
+            this.comboBox3.Size = new System.Drawing.Size(120, 30);
+            this.comboBox3.TabIndex = 6;
+            // 
+            // dateTimePicker1
+            // 
+            this.dateTimePicker1.CustomFormat = "";
+            this.dateTimePicker1.Font = new System.Drawing.Font("Microsoft Sans Serif", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.dateTimePicker1.Location = new System.Drawing.Point(610, 19);
+            this.dateTimePicker1.Margin = new System.Windows.Forms.Padding(2);
+            this.dateTimePicker1.Name = "dateTimePicker1";
+            this.dateTimePicker1.Size = new System.Drawing.Size(189, 28);
+            this.dateTimePicker1.TabIndex = 5;
+            // 
+            // comboBox2
+            // 
+            this.comboBox2.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.comboBox2.DropDownWidth = 350;
+            this.comboBox2.Font = new System.Drawing.Font("Microsoft Sans Serif", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.comboBox2.FormattingEnabled = true;
+            this.comboBox2.Location = new System.Drawing.Point(226, 16);
+            this.comboBox2.Margin = new System.Windows.Forms.Padding(2);
+            this.comboBox2.Name = "comboBox2";
+            this.comboBox2.Size = new System.Drawing.Size(215, 30);
+            this.comboBox2.TabIndex = 4;
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.comboBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Items.AddRange(new object[] {
+            "КЭПИ",
+            "ИГН",
+            "ИЕН",
+            "ИИЯ",
+            "ИИП",
+            "ИКИ",
+            "МИ",
+            "ИПП",
+            "ИСТ",
+            "ИТНИТ",
+            "ИЭУ",
+            "ЮИ",
+            "ИНО"});
+            this.comboBox1.Location = new System.Drawing.Point(4, 16);
+            this.comboBox1.Margin = new System.Windows.Forms.Padding(2);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(218, 30);
+            this.comboBox1.TabIndex = 3;
+            this.comboBox1.SelectedIndexChanged += new System.EventHandler(this.comboBox1_SelectedIndexChanged);
+            // 
+            // cueTextBox2
+            // 
+            this.cueTextBox2.Cue = "Номер зачётной книжки";
+            this.cueTextBox2.Font = new System.Drawing.Font("Microsoft Sans Serif", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.cueTextBox2.Location = new System.Drawing.Point(4, 54);
+            this.cueTextBox2.Margin = new System.Windows.Forms.Padding(2);
+            this.cueTextBox2.Name = "cueTextBox2";
+            this.cueTextBox2.Size = new System.Drawing.Size(261, 28);
+            this.cueTextBox2.TabIndex = 2;
+            // 
+            // cueTextBox1
+            // 
+            this.cueTextBox1.Cue = "ФИО студента";
+            this.cueTextBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.cueTextBox1.Location = new System.Drawing.Point(269, 54);
+            this.cueTextBox1.Margin = new System.Windows.Forms.Padding(2);
+            this.cueTextBox1.Name = "cueTextBox1";
+            this.cueTextBox1.Size = new System.Drawing.Size(334, 28);
+            this.cueTextBox1.TabIndex = 1;
+            // 
+            // button1
+            // 
+            this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.button1.Location = new System.Drawing.Point(817, 392);
+            this.button1.Margin = new System.Windows.Forms.Padding(2);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(92, 28);
+            this.button1.TabIndex = 2;
+            this.button1.Text = "Добавить";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.button2.Location = new System.Drawing.Point(913, 392);
+            this.button2.Margin = new System.Windows.Forms.Padding(2);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(85, 28);
+            this.button2.TabIndex = 3;
+            this.button2.Text = "Удалить";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button3
+            // 
+            this.button3.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.button3.Location = new System.Drawing.Point(817, 430);
+            this.button3.Margin = new System.Windows.Forms.Padding(2);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(181, 28);
+            this.button3.TabIndex = 4;
+            this.button3.Text = "Сохранить";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.DarkRed;
+            this.ClientSize = new System.Drawing.Size(1006, 474);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.dataGridView1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Margin = new System.Windows.Forms.Padding(2);
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "Form1";
+            this.Text = "Тык";
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private CueTextBox cueTextBox1;
+        private CueTextBox cueTextBox2;
+        private System.Windows.Forms.ComboBox comboBox3;
+        private System.Windows.Forms.DateTimePicker dateTimePicker1;
+        private System.Windows.Forms.ComboBox comboBox2;
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.DataGridViewTextBoxColumn recordBook;
+        private System.Windows.Forms.DataGridViewTextBoxColumn fullName;
+        private System.Windows.Forms.DataGridViewTextBoxColumn department;
+        private System.Windows.Forms.DataGridViewTextBoxColumn specification;
+        private System.Windows.Forms.DataGridViewTextBoxColumn dateOfAdmission;
+        private System.Windows.Forms.DataGridViewTextBoxColumn group;
+
+
+    }
+}
+
---
+++ lab7/Form1.cs
@@ -0,0 +1,154 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace lab7
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        List<Itnit> lItnit = new List<Itnit>
+        {
+            new Itnit(){Id=1,Name="Прикладная математика и информатика"},
+            new Itnit(){Id=2,Name="Математика и компьютерные науки"},
+            new Itnit(){Id=3,Name="Физика"},
+            new Itnit(){Id=4,Name="Радиофизика"},
+            new Itnit(){Id=5,Name="Прикладная информатика"},
+            new Itnit(){Id=6,Name="Информационная безопасность"},
+            new Itnit(){Id=7,Name="Техносферная безопасность"},
+            new Itnit(){Id=8,Name="Профессиональное обучение"},
+            new Itnit(){Id=9,Name="Педагогическое образование "},
+        };
+        List<Ui> lUi = new List<Ui>
+        {
+            new Ui(){Id=0,Name="Юриспруденция"}
+        };
+        class Institut 
+        {
+            public int Id { get; set; }
+            public string Name { get; set; }
+        }
+
+        class Itnit : Institut { }
+        class Ui:Institut { }
+
+        private void comboBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            switch (comboBox1.SelectedIndex)
+            {
+                
+                case 9:
+                    {
+                        comboBox2.DataSource = lItnit;
+                        comboBox2.ValueMember= "Id";
+                        comboBox2.DisplayMember= "Name";
+                        break;
+                    }
+                case 11: 
+                    {
+                        comboBox2.DataSource = lUi;
+                        comboBox2.ValueMember = "Id";
+                        comboBox2.DisplayMember = "Name";
+                        break;
+                    }
+
+            }
+        }
+
+        private void dataGridView1_MouseClick(object sender, MouseEventArgs e)
+        {
+            try {
+                if (dataGridView1.Rows.Count==0 )
+                {
+                    throw new Exception();
+                }
+                else {
+                    comboBox3.Text = "";
+                    cueTextBox2.Text = dataGridView1.Rows[dataGridView1.CurrentRow.Index].Cells[0].Value.ToString();
+                    cueTextBox1.Text = dataGridView1.Rows[dataGridView1.CurrentRow.Index].Cells[1].Value.ToString();
+                    comboBox1.SelectedText = dataGridView1.Rows[dataGridView1.CurrentRow.Index].Cells[2].Value.ToString();
+                    comboBox2.SelectedText = dataGridView1.Rows[dataGridView1.CurrentRow.Index].Cells[3].Value.ToString();
+                    comboBox3.SelectedText = dataGridView1.Rows[dataGridView1.CurrentRow.Index].Cells[5].Value.ToString();
+                    dateTimePicker1.Text = dataGridView1.Rows[dataGridView1.CurrentRow.Index].Cells[4].Value.ToString();
+                }
+            }
+            catch (Exception ex) { MessageBox.Show("Ошибка! Выделена пустая строка"); }
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            int flag = 0;
+            if (cueTextBox1.Text != "" && cueTextBox2.Text != "" && comboBox1.Text != "" && comboBox2.Text != "" && comboBox3.Text != "")
+            {
+                for (int i = 0; i < dataGridView1.Rows.Count; i++)
+                {
+                    if (cueTextBox2.Text == dataGridView1.Rows[i].Cells[0].Value.ToString())
+                    {
+                        flag = 1;
+                    }
+                }
+                if (flag == 0)
+                {
+                    dataGridView1.Rows.Add(cueTextBox2.Text, cueTextBox1.Text, comboBox1.Text, comboBox2.Text, dateTimePicker1.Value, comboBox3.Text);
+                }
+                else { MessageBox.Show("Такой номер книжки уже есть","Предупреждение"); }
+            }
+            else { MessageBox.Show("Заполните все поля", "Ошибка!"); }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                if (dataGridView1.Rows.Count!=0)
+                {
+                    dataGridView1.Rows.RemoveAt(dataGridView1.CurrentRow.Index);
+                }
+            }
+            catch (Exception ex) { MessageBox.Show("Ошибка! Выделена незаполненная строка"); }
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                if (dataGridView1.Rows.Count != 0)
+                {
+                    int flag = 0;
+                    for (int i = 0; i < dataGridView1.Rows.Count; i++)
+                    {
+                        if (cueTextBox2.Text == dataGridView1.Rows[i].Cells[0].Value.ToString())
+                        {
+                            flag = 1;
+                        }
+                    }
+                    if (flag == 0) { 
+                        if (cueTextBox1.Text != "" && cueTextBox2.Text != "" && comboBox1.Text != "" && comboBox2.Text != "" && comboBox3.Text != "")
+                        {
+                            dataGridView1.Rows[dataGridView1.CurrentRow.Index].Cells[0].Value = cueTextBox2.Text;
+                            dataGridView1.Rows[dataGridView1.CurrentRow.Index].Cells[1].Value = cueTextBox1.Text;
+                            dataGridView1.Rows[dataGridView1.CurrentRow.Index].Cells[2].Value = comboBox1.Text;
+                            dataGridView1.Rows[dataGridView1.CurrentRow.Index].Cells[3].Value = comboBox2.Text;
+                            dataGridView1.Rows[dataGridView1.CurrentRow.Index].Cells[4].Value = dateTimePicker1.Value;
+                            dataGridView1.Rows[dataGridView1.CurrentRow.Index].Cells[5].Value = comboBox3.Text;
+                        }
+                        else { MessageBox.Show("Строка не выбрана", "Ошибка!"); } 
+                    }
+                    else { MessageBox.Show("Такой номер книжки уже есть", "Предупреждение"); }
+                }
+            }
+            catch(Exception ex) { MessageBox.Show("Строка не выбрана", "Ошибка!"); }
+        }
+    }
+}
