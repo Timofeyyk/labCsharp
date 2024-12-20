305215d7a979c42f47a622b36dc8849c2fcd706f
Timofey Kuratov <nynesnayu@gmail.com>
2024-12-20 16:12:19 +0300

    Дописаны модели

--- lab10/Models/UserModel.cs
+++ lab10/Models/UserModel.cs
@@ -16,10 +16,13 @@ namespace lab10.Models
         {
             using (SqlConnection conn = new SqlConnection(connectionString))
             {
-                string query = "INSERT INTO roles (rolename) VALUES (@rolename)";
+                string query = "INSERT INTO users (UserLogin,UserPassword,UserRole,UserName) VALUES (@UserLogin,@UserPassword,@UserRole,@UserName)";
                 SqlCommand cmd = new SqlCommand(query, conn);
                 cmd.Parameters.Clear();
-                cmd.Parameters.AddWithValue("@rolename", RoleName);
+                cmd.Parameters.AddWithValue("@UserLogin", UserLogin);
+                cmd.Parameters.AddWithValue("@UserPassword", UserPassword);
+                cmd.Parameters.AddWithValue("@UserRole", UserRole);
+                cmd.Parameters.AddWithValue("@UserName", UserName);
                 conn.Open();
                 cmd.ExecuteNonQuery();
             }
@@ -29,7 +32,7 @@ namespace lab10.Models
         {
             using (SqlConnection conn = new SqlConnection(connectionString))
             {
-                string query = "SELECT * FROM roles";
+                string query = "SELECT * FROM users";
                 SqlDataAdapter adapter = new SqlDataAdapter(query, conn);
                 DataTable dataTable = new DataTable();
                 adapter.Fill(dataTable);
@@ -40,10 +43,10 @@ namespace lab10.Models
         {
             using (SqlConnection conn = new SqlConnection(connectionString))
             {
-                string query = "DELETE FROM roles WHERE roleid = @roleid";
+                string query = "DELETE FROM users WHERE UserId = @UserId";
                 SqlCommand cmd = new SqlCommand(query, conn);
                 cmd.Parameters.Clear();
-                cmd.Parameters.AddWithValue("@roleid", RoleId);
+                cmd.Parameters.AddWithValue("@UserId", UserId);
                 conn.Open();
                 cmd.ExecuteNonQuery();
             }
@@ -52,11 +55,14 @@ namespace lab10.Models
         {
             using (SqlConnection conn = new SqlConnection(connectionString))
             {
-                string query = "UPDATE roles SET rolename = @rolename WHERE orderID = @roleID";
+                string query = "UPDATE users SET UserLogin = @UserLogin, UserPassword = @UserPassword, UserRole = @UserRole, UserName = @UserName WHERE UserId = @UserId";
                 SqlCommand cmd = new SqlCommand(query, conn);
                 cmd.Parameters.Clear();
-                cmd.Parameters.AddWithValue("@roleid", RoleId);
-                cmd.Parameters.AddWithValue("@rolename", RoleName);
+                cmd.Parameters.AddWithValue("@UserId", UserId);
+                cmd.Parameters.AddWithValue("@UserLogin", UserLogin);
+                cmd.Parameters.AddWithValue("@UserPassword", UserPassword);
+                cmd.Parameters.AddWithValue("@UserRole", UserRole);
+                cmd.Parameters.AddWithValue("@UserName", UserName);
                 conn.Open();
                 cmd.ExecuteNonQuery();
             }
